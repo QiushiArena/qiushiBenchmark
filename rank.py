39509a354f30a747a37ff24623e9eaf20275a0b3
galgmh <2335345445@qq.com>
2026-04-15 18:42:05 +0800

    bino

--- rank.py
+++ rank.py
@@ -11,9 +11,9 @@ from detectors.common import DataLoader, Evaluator
 
 results = []
 scores = []
-labels = [0] * 2500 + [1] * 2500
-'''
-with open("gpt_iter_yelp_train_min_01234.json", "r", encoding="utf-8") as f:
+labels = [0] * 150 + [1] * 150
+
+with open("b_iter_yelp_train_min_01234.json", "r", encoding="utf-8") as f:
     results = json.load(f)
 
 #print(len(results))
@@ -27,8 +27,9 @@ scores_2 = scores[0:150] + scores[300:450]
 scores_3 = scores[0:150] + scores[450:600]
 scores_4 = scores[0:150] + scores[600:750]
 scores_1 = scores[0:150] + scores[150:300]
-'''
 
+
+'''
 with open("fast_data/iterate_results.json", "r", encoding="utf-8") as f:
     results = json.load(f)    
 
@@ -36,6 +37,7 @@ scores_1 = [s["human"]["ai_prob"] for s in results if (s["domain"] == "writing_p
 scores_2 = [s["human"]["ai_prob"] for s in results if (s["domain"] == "writing_prompt")] + [s["llm_iterations"]["2"]["ai_prob"] for s in results if (s["domain"] == "writing_prompt")]
 scores_3 = [s["human"]["ai_prob"] for s in results if (s["domain"] == "writing_prompt")] + [s["llm_iterations"]["3"]["ai_prob"] for s in results if (s["domain"] == "writing_prompt")]
 scores_4 = [s["human"]["ai_prob"] for s in results if (s["domain"] == "writing_prompt")] + [s["llm_iterations"]["4"]["ai_prob"] for s in results if (s["domain"] == "writing_prompt")]
+'''
 
 evaluator = Evaluator()
 detection_result_1 = evaluator.evaluate(scores_1, labels)
